be702a72f571083a0cac87ced5c9be2dc8c148bf
Mauro Estevez <mauro@mambopublicidad.com>
2020-06-30 22:17:50 -0300

    Commit test

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // JUEGO
 var quiz = [
     {
-      pregunta: 'Cuál es el nombre del planeta rojo?',
+      pregunta: 'Cómo es el nombre del planeta rojo?',
       respuesta: 'marte',
     },
     {
